985eda6a08e5105f7fc8cd46088cdbbdbe5ec9b8
Akila-Piumal <akilapiumal16@gmail.com>
2022-10-09 10:30:34 +0530

    automatically set current date to date field.

--- assignments/JS/SPA/POS/controller/PlaceOrderController.js
+++ assignments/JS/SPA/POS/controller/PlaceOrderController.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
+    autoGenerateOrderID();
+
+    $('#dateInput').val(getCurrentDate());
+});
+
+function autoGenerateOrderID() {
     if ($.isEmptyObject(orders)) {
         $('#orderId').val('O001');
     } else {
@@ -9,4 +15,22 @@ $(document).ready(function () {
         let newID = idGenerator(lastId);
         $('#orderId').val(newID);
     }
-});
+}
+
+function getCurrentDate() {
+
+    function padTo2Digits(num) {
+        return num.toString().padStart(2, '0');
+    }
+
+    function formatDate(date = new Date()) {
+        return [
+            date.getFullYear(),
+            padTo2Digits(date.getMonth() + 1),
+            padTo2Digits(date.getDate()),
+        ].join('-');
+    }
+
+    return formatDate();
+}
+
